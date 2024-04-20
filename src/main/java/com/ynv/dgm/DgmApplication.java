b061ef985c422cac73168afb9e60624e5813c554
NikitaYarosh <yarosh.nv@yandex.ru>
2024-04-20 13:31:54 +0300

    feature: add spring initializer template

---
+++ src/main/java/com/ynv/dgm/DgmApplication.java
@@ -0,0 +1,13 @@
+package com.ynv.dgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgmApplication.class, args);
+	}
+
+}
